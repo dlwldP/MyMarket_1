358777396895a8d33b108e1f92a2861e13e0d331
Lee-jiye <80204749+Lee-jiye@users.noreply.github.com>
2023-11-22 20:51:05 +0900

    Welcome Hwangsonglee 마켓! 추가 및 색 수정

--- app/src/main/java/com/example/mymarket/MainActivity.kt
+++ app/src/main/java/com/example/mymarket/MainActivity.kt
@@ -3,13 +3,22 @@ package com.example.mymarket
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.mymarket.ui.theme.MyMarketTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,9 +29,9 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = Color(0xFFFFA500) // 주황색
                 ) {
-                    Greeting("Android")
+                    Greeting("Hwangsonglee market", Modifier.background(color = Color.Green).padding(16.dp).fillMaxWidth())
                 }
             }
         }
@@ -32,8 +41,15 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Welcome $name!",
         modifier = modifier
+            .background(color = Color.Green) // 초록색 배경
+            .padding(16.dp) // 여백 추가
+            .fillMaxWidth() // 최대 너비로 채우기
+            .border(2.dp, Color.Black) // 테두리 추가
+            .clip(RoundedCornerShape(8.dp)) // 둥근 모서리 적용
+            .padding(8.dp), // 내부 여백 추가
+        fontWeight = FontWeight.Bold // 볼드체로 설정
     )
 }
 
@@ -41,6 +57,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     MyMarketTheme {
-        Greeting("Android")
+        Greeting("Hwangsonglee 마켓")
     }
 }
